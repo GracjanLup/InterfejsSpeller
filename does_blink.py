c4e58064811068ddcea1cbab99371619955ebbb9
GracjanLup <gracjan.popiolkowski@gmail.com>
2022-11-29 00:36:39 +0100

    Create does_blink.py

---
+++ does_blink.py
@@ -0,0 +1,19 @@
+czestotliwosc = [2,4,5]
+counter = -1
+
+
+def does_blink():
+    while True:
+        #czestotliwosc.append()
+        counter+1
+        x = 0
+        if counter>=20:
+            for i in range(8):
+                if czestotliwosc[counter - i + 1] > czestotliwosc[counter - i]:
+                    x+1
+                    if x == 8 and czestotliwosc[conter - 2*i] < -10000:
+                        x = 0
+                        return True;
+
+
+does_blink()
